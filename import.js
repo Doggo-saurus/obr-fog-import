1d1b91e7e6db301a6581f2cddd3e7aede150664c
Doggosaurus <doggosaurus69@protonmail.com>
2023-09-12 21:37:11 +1000

    Add map alignment

--- import.js
+++ import.js
@@ -3,10 +3,47 @@ import OBR, { buildCurve } from "@owlbear-rodeo/sdk";
 const FOW_ID = "com.armindoflores.fogofwar";
 const ID = "com.whatever.import";
 
-export function importFog(import_data_str, import_dpi_str, error_element) {
+export async function updateMaps(map_align) {
+  const maps = await OBR.scene.items.getItems((item) => item.layer === "MAP");
+  const map_map = {};
+  for(var i = 0; i < maps.length; i++) {
+    let map_opt = map_align.querySelector('#map-'+maps[i].id);
+    if (map_opt === null) {
+      map_opt = document.createElement('option');
+      map_opt.id = 'map-' + maps[i].id;
+      map_opt.value = maps[i].id;
+      map_opt.innerText = maps[i].name;
+      map_align.appendChild(map_opt);
+    }
+    map_map[maps[i].id] = maps[i].name;
+  }
+
+  let map_opts = map_align.querySelectorAll('option');
+  for(var i = 0; i < map_opts.length; i++) {
+    if (map_map[map_opts[i].value] === undefined) {
+      map_align.removeChild(map_opts[i]);
+//      console.log("Removing "+ map_opts[i].value);
+    }
+  }
+};
+
+export async function importFog(import_data_str, import_dpi_str, import_map_align, error_element) {
 
   error_element.innerText = '';
   let import_data, import_dpi;
+
+  let import_map;
+  let offset_x = 0, offset_y = 0;
+
+  // getItems for the uuid of the map, offset the points accordingly (after dpi scaling)
+  if (import_map_align !== "") {
+    import_map = await OBR.scene.items.getItems([import_map_align]);
+    if (import_map.length > 0) {
+      offset_x = import_map[0].position.x;
+      offset_y = import_map[0].position.y;
+    }
+  }
+
   try {
     import_data = JSON.parse(import_data_str);
     import_dpi = parseInt(import_dpi_str);
@@ -35,7 +72,7 @@ export function importFog(import_data_str, import_dpi_str, error_element) {
   for (var i = 0; i < import_data.walls.length; i++) {
     const line = buildCurve()
       .tension(0)
-		  .points([{x: import_data.walls[i].c[0]*scale, y: import_data.walls[i].c[1]*scale}, {x:import_data.walls[i].c[2]*scale,y:import_data.walls[i].c[3]*scale}])
+		  .points([{x: import_data.walls[i].c[0]*scale + offset_x, y: import_data.walls[i].c[1]*scale + offset_y}, {x:import_data.walls[i].c[2]*scale + offset_x,y:import_data.walls[i].c[3]*scale + offset_y}])
       .fillColor("#000000")
       .fillOpacity(0)
       .layer("DRAWING")
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import { importFog } from './import.js'
+import { importFog, updateMaps } from './import.js'
 import OBR from "@owlbear-rodeo/sdk";
 
 document.querySelector('#app').innerHTML = `
@@ -11,6 +11,7 @@ document.querySelector('#app').innerHTML = `
       <textarea id="import_data" style="width:200px;height:100px;"></textarea>
       </p>
       <p>DPI <input id="import_dpi" type="text" value="150" size="1" maxlength="4"></p>
+      <p>Map Alignment <select id="map_align"><option selected>Loading..</option></select></p>
       <button id="import" type="button">Import!</button>
       <p id="error_text"></p>
     </div>
@@ -19,5 +20,17 @@ document.querySelector('#app').innerHTML = `
 
 //setupCounter(document.querySelector('#counter'))
 OBR.onReady(() => {
-  document.querySelector('#import').addEventListener("click", () => importFog(document.querySelector("#import_data").value, document.querySelector("#import_dpi").value, document.querySelector("#error_text")));
+  document.querySelector('#import').addEventListener("click", () => importFog(document.querySelector("#import_data").value, document.querySelector("#import_dpi").value, document.querySelector("#map_align").value, document.querySelector("#error_text")));
+  updateMapTimeout(3000);
 });
+
+function updateMapTimeout(timeout) {
+  setTimeout(() => {
+    try {
+      updateMaps(document.querySelector('#map_align'));
+      updateMapTimeout(10000);
+    } catch {
+
+    }
+  }, timeout);
+}
